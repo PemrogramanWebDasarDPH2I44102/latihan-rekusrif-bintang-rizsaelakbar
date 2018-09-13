b93a459eab6f6540d52e8e0873706287e32df982
Rizsa El Akbar <38714405+rizsaelakbar@users.noreply.github.com>
2018-09-13 08:16:07 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,39 @@
 <?php
-
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
+$batasnya = 5;
+function rekursif($i = 1){
+  global $batasnya;
+  echo "*";
+  $i++;
+  if($i<=$batasnya){
     rekursif($i);
+  }
+  else{
+    echo"<br>";
+    $batas--;
+    if ($batasnya != 0){
+      rekursif();
+    }
+  }
 }
-rekursif(5);
+rekursif();
+/*====================================================================================*/
+echo "<br>";
+
+$batas = 1;
+function rekursif2($x = 1){
+  global $batas;
+  echo "$x";
+  $x++;
+    if ($x<=$batas){
+      rekursif2($x);
+    }
+    else{
+      echo"<br>";
+      $batas++;
+      if ($batas <= 5){
+        rekursif2();
+      }
+    }
+}
+rekursif2();
 ?>
